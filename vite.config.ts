5861fed440d7707cac2b81f9b0a7765d93ab5af6
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-10-23 17:38:02 +0200

    chore: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [],
   build: {
     rollupOptions: {
-      external: ["/@lithium\/.*/", "/@sodium\/.*/", "/@lucide\/.*/"],
+      external: [/@(lithium|sodium|lucide)\/.*/],
     },
   },
 });
