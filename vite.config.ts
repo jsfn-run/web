c5a0fb26a5bb670c069aca7c698adcb682feceeb
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-10-23 14:00:47 +0200

    chore: fix rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: './assets',
+  publicDir: "./assets",
   plugins: [],
+  build: {
+    rollupOptions: {
+      external: ["@lithium/*", "@sodium/*", "@lucide/*"],
+    },
+  },
 });
